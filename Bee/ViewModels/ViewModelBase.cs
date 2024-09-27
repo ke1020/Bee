9947e7ba735dfe4527b5db63b6459645b1bbf62e
xkaspx <xkaspx@aliyun.com>
2024-09-27 16:59:10 +0800

    first

---
+++ Bee/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Bee.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ docs/__CLI.md
@@ -0,0 +1,11 @@
+# 相关 CLI 命令
+
+```bash
+
+```
+
+## 卸载 Avalonia 模板
+
+```bash
+dotnet new uninstall Avalonia.Templates
+```
